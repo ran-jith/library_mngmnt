eff51482733846324b63ef635511296678187d1c
athirasdas <44548288+athirasdas@users.noreply.github.com>
2018-10-29 22:36:58 +0530

    Update Display.java

--- OLM/src/java/Display.java
+++ OLM/src/java/Display.java
@@ -20,7 +20,7 @@ public class Display extends HttpServlet {
         String dbName = "olm";
         String driver = "com.mysql.jdbc.Driver";
         String userName = "root";
-        String password = "password123";
+        String password = "root";
  
         Statement st;
         try {
